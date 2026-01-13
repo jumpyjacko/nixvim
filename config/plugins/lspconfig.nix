53d4851d32ccc122576bb44e37a0be53ead5a027
Jackson Ly <bobjacklee27@gmail.com>
2026-01-13 12:47:36 +1100

    fix: lsp

--- config/plugins/lspconfig.nix
+++ config/plugins/lspconfig.nix
@@ -1,21 +1,19 @@
 {
-  plugins.lspconfig = {
-    enable = true;
-    servers = {
-      nixd.enable = true;
-      clangd.enable = true;
-      cssls.enable = true;
-      jsonls.enable = true;
-      lua_ls.enable = true;
-      marksman.enable = true;
-      omnisharp.enable = true;
-      rust_analyzer.enable = true;
-      svelte.enable = true;
-      tailwindcss.enable = true;
-      tinymist.enable = true;
-      ts_ls.enable = true;
-      typstyle.enable = true;
-      yamlls.enable = true;
-    };
+  plugins.lspconfig.enable = true;
+  lsp.servers = {
+    nixd.enable = true;
+    clangd.enable = true;
+    cssls.enable = true;
+    jsonls.enable = true;
+    lua_ls.enable = true;
+    marksman.enable = true;
+    omnisharp.enable = true;
+    rust_analyzer.enable = true;
+    svelte.enable = true;
+    tailwindcss.enable = true;
+    tinymist.enable = true;
+    ts_ls.enable = true;
+    typstyle.enable = true;
+    yamlls.enable = true;
   };
 }
