b8c4ecc88e25a04d0180a9397bbb3b6babd24488
Jackson Ly <bobjacklee27@gmail.com>
2026-01-13 13:31:02 +1100

    fix hash

--- config/plugins/oldworld.nix
+++ config/plugins/oldworld.nix
@@ -9,7 +9,7 @@
           owner = "dgox16";
           repo = "oldworld.nvim";
           rev = "db072a0bd2c90035fc3a57a73014cb54d3902f1f";
-          hash = "sha256-0E9Nj4XB7Kum/hj1RlN2Oz6IQFD2y7FboFmbOOGGmXc=";
+          hash = "sha256-yO5XKSMwDu0/QYnoMbxWs+h0tfjftAYJYPrKO2XYfNQ=";
         };
       };
     }
