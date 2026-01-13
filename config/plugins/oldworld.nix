bfe15db370067fa5bbac17bf479ce83ae5e8aee8
Jackson Ly <bobjacklee27@gmail.com>
2026-01-13 13:26:46 +1100

    are we actually joking?

--- config/plugins/oldworld.nix
+++ config/plugins/oldworld.nix
@@ -5,7 +5,7 @@
     {
       plugin = pkgs.vimUtils.buildVimPlugin {
         name = "oldworld";
-        src = pkgs.fetchFromGithub {
+        src = pkgs.fetchFromGitHub {
           owner = "dgox16";
           repo = "oldworld.nvim";
           rev = "db072a0bd2c90035fc3a57a73014cb54d3902f1f";
